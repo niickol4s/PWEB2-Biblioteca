5ef07f3330d6786059e2c0aa00d1243449e1bf38
Nickolas Davi <105878022+niickol4s@users.noreply.github.com>
2024-11-30 09:55:50 -0300

    Add files via upload

---
+++ web2/acervo.js
@@ -0,0 +1,9 @@
+class Acervo {
+    constructor(categoria, autoria, data_publicacao) {
+        this._categoria = categoria;
+        this._autoria = autoria;
+        this._data_publicacao = data_publicacao;
+    }
+
+    
+}
---
+++ web2/biblioteca.js
@@ -0,0 +1,7 @@
+class Biblioteca {
+    constructor(id, nome, endereco) {
+        this._id = id;
+        this._nome = nome;
+        this._endereco = endereco;
+    }
+}
